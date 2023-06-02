8628d16e2a13da883fd0155d55d35f92e16d63c0
Diarassouba Mamadou <couda.dm@gmail.com>
2023-06-02 00:22:58 +0000

    upate

--- frontend/src/components/List.jsx
+++ frontend/src/components/List.jsx
@@ -4,15 +4,19 @@ import { Link } from "react-router-dom";
 function List() {
     console.log(data)
   return (
-     <div className="mt-5 card">
+     <div>
       {data.map(item => {
+        
+        const title = item.title.split(' ').join('-')
+        const content = `${item.Content.substring(0,100)}.....`
+        
         return (
-            <>
-                <div key={item}>
-                <h2><Link to={`/post/:${item.title}`}>{item.title}</Link></h2>
-                <p>{item.Content}</p>
+          
+                <div key={item._id} className="mt-5 card">
+                  <h2><Link to={`/post/:${title}`} state={{id : item._id}}>{item.title}</Link></h2>
+                  <p>{content}</p>
                 </div>
-            </>
+            
         )
       })}
      </div>
--- frontend/src/pages/Single.jsx
+++ frontend/src/pages/Single.jsx
@@ -1,6 +1,18 @@
+import { useLocation } from "react-router-dom";
+import data from '../data';
 function Single() {
+  // REcuperation de l'id passer en State dans List.jsx
+  const {state : {id}} = useLocation()
+  // Recuperer l'objet en verifiant avac l'id
+  const post = data.find(post => post._id === id)
+ 
+
   return (
-      <p>Single Page</p>
+    <>
+        <h3>{post.title}</h3>
+        <h3>{post.Content}</h3>
+    
+    </>
   ) 
 }
 
