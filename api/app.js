60608d42438cb0ca5c4a19bf9be3379420832258
Diarassouba Mamadou <couda.dm@gmail.com>
2023-06-19 16:27:05 +0000

    dmeo

--- api/app.js
+++ api/app.js
@@ -3,9 +3,13 @@ import doten from "dotenv";
 import cors from "cors";
 import mongoose from "mongoose";
 import routes from "./routes";
+import path from "path"
+
 const app = express();
 const PORT = 3000;
 app.use(cors());
+
+app.use(express.static('dist'))
 
 // SI le ficher .env est a la racine
 doten.config();
@@ -25,3 +29,9 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+  if(process.env.NODE_ENV === "production"){
+      app.get('/*', (_, res)=>{
+          res.sendFile(path.join(__dirname,'../dist/index.html'))
+      })
+  }
